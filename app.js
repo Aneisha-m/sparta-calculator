c689dbfd9100474afac31698f12e30f769e8fc71
Aneisha-m <AMallikaratchy@spartaglobal.com>
2019-01-14 15:17:19 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("working");
+
+prompt("Welcome to my JS Calculator\nDo you want to use the Basic Calculator(Type Basic to select option) or the Advanced Calculator(Type Adv to select option) ");
+
+// ===Basic Calculator===
